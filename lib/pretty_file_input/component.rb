2e6e5fc4a9b670dc69d2a493b7e71d5fa385d9ff
ajb <ad@mbecker.cc>
2015-03-19 11:20:14 -0400

    take2

--- lib/pretty_file_input/component.rb
+++ lib/pretty_file_input/component.rb
@@ -20,14 +20,14 @@ class PrettyFileInput::Component < Erector::Widget
       }.to_json
     ) {
       div.js_pfi_toggle.pfi_file_wrapper(style: @filename ? nil : 'display:none;') {
-        div.input_group {
+        div.pfi_input {
           span.js_pfi_filename @filename
           a.button.info.js_pfi_remove 'Remove'
         }
       }
       label.js_pfi_toggle.pfi_file_wrapper(style: @filename ? 'display:none;' : nil) {
         input type: 'file'
-        div.input_group {
+        div.pfi_input {
           span.js_pfi_filename.js_pfi_status 'Choose a file...'
           a.button.info.js_pfi_browse 'Browse'
         }
